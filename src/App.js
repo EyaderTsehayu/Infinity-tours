43162e41ba347509ed3c1a8d5eac7b4ac06e9087
Eyader Tsehayu <eyaderts@gmail.com>
2022-12-22 20:51:04 +0300

    update by addng navigation on top of all renders

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
 import Users from "./user/pages/Users.jsx";
 import NewPlace from "./places/pages/NewPlace.jsx";
 
 import "./App.css";
+import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Users />} exact />
-      <Route path="/places/new" element={<NewPlace />} exact />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <BrowserRouter>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path="/" element={<Users />} exact />
+          <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
   );
 }
 
