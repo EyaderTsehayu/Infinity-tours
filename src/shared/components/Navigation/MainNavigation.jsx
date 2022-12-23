a2ca173e8282d4b46661113ba97423429d586b4b
Eyader Tsehayu <eyaderts@gmail.com>
2022-12-23 21:44:45 +0300

    add some react portals for mobile version usage

--- src/shared/components/Navigation/MainNavigation.jsx
+++ src/shared/components/Navigation/MainNavigation.jsx
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import MainHeader from "./MainHeader";
 import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import Backdrop from "../UIElements/Backdrop";
 
-const MainNavigation = () => {
+const MainNavigation = (props) => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <React.Fragment>
-      <SideDrawer>
+      {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
         <nav className="main-navigation__drawer-nav">
           <NavLinks />
         </nav>
       </SideDrawer>
+
       <MainHeader>
-        <button className="main-navigation__menu-btn">
+        <button
+          className="main-navigation__menu-btn"
+          onClick={openDrawerHandler}
+        >
           <span />
           <span />
           <span />
