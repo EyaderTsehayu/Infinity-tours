67f2cfc8ec00639fb7909d6a0a3d73a820e5e52b
Eyader Tsehayu <eyaderts@gmail.com>
2022-12-25 22:36:24 +0300

    add user place Route

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
 import Users from "./user/pages/Users.jsx";
 import NewPlace from "./places/pages/NewPlace.jsx";
-
+import UserPlaces from "./places/pages/UserPlaces.jsx";
 import "./App.css";
 import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
 
@@ -15,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Users />} exact />
           <Route path="/places/new" element={<NewPlace />} exact />
+          <Route path="/:userId/places" element={<UserPlaces />} exact />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </main>
