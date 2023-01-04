e2a975951b3c2cd264715e4f1f250ae9f5b5e3dd
Eyader Tsehayu <eyaderts@gmail.com>
2023-01-04 21:07:56 +0300

    render input form component

--- src/places/pages/NewPlace.jsx
+++ src/places/pages/NewPlace.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Input from "../../shared/FormElements/Input";
+
+import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <div>NewPlace is working!!</div>;
+  return (
+    <div className="place-form">
+      <Input element="input" type="text" label="Title" />
+    </div>
+  );
 };
 
 export default NewPlace;
