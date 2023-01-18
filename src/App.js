85d2fb4c46d806ef41eb8e106596774ffe2710bd
Eyader Tsehayu <eyaderts@gmail.com>
2023-01-18 21:16:59 +0300

    add auth context and implement with app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { Route, Routes, Navigate, BrowserRouter } from "react-router-dom";
 
 import Users from "./user/pages/Users.jsx";
@@ -8,22 +8,34 @@ import "./App.css";
 import MainNavigation from "./shared/components/Navigation/MainNavigation.jsx";
 import UpdatePlace from "./places/pages/UpdatePlace.jsx";
 import Auth from "./user/pages/Auth.jsx";
-
+import { AuthContext } from "./shared/context/auth-context.js";
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const login = useCallback(() => {
+    setIsLoggedIn(true);
+  }, []);
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+  }, []);
+
   return (
-    <BrowserRouter>
-      <MainNavigation />
-      <main>
-        <Routes>
-          <Route path="/" element={<Users />} exact />
-          <Route path="/:userId/places" element={<UserPlaces />} exact />
-          <Route path="/places/new" element={<NewPlace />} exact />
-          <Route path="/places/:placeId" element={<UpdatePlace />} />
-          <Route path="/auth" element={<Auth />} exact />
-          <Route path="*" element={<Navigate to="/" replace />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
+    <AuthContext.Provider
+      value={{ isLoggedIn: isLoggedIn, login: login, logout: logout }}
+    >
+      <BrowserRouter>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" element={<Users />} exact />
+            <Route path="/:userId/places" element={<UserPlaces />} exact />
+            <Route path="/places/new" element={<NewPlace />} exact />
+            <Route path="/places/:placeId" element={<UpdatePlace />} />
+            <Route path="/auth" element={<Auth />} exact />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+        </main>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
